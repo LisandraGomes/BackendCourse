29597299461cbe870bd8db8f0b41af88444282ad
LisandraGomes <lisandragomes53@gmail.com>
2023-03-14 21:19:24 -0300

    Subindo inicio do projeto da parte de Dapper.

---
+++ BaltaDapper/BaltaDapper/Controller/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaltaDapper.Controller
+{
+    public class DefaultController
+    {
+   
+        
+        public static void Get()
+        {
+
+        }
+    }
+}
---
+++ BaltaDapper/BaltaDapper/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+const string connectionString = "";
+using (var connection = new SqlConnection(connecionString)) { }
