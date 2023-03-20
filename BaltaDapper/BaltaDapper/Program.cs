cdf8cd76cc527e994fb7e3d83bf91b97501455d5
LisandraGomes <lisandragomes53@gmail.com>
2023-03-20 06:13:37 -0300

    Inicializando a connection string, após configurar o Docker Azure.

--- BaltaDapper/BaltaDapper/Program.cs
+++ BaltaDapper/BaltaDapper/Program.cs
@@ -1,4 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-const string connectionString = "";
-using (var connection = new SqlConnection(connecionString)) { }
+﻿using Microsoft.Data.SqlClient;
+// See https://aka.ms/new-console-template for more information
+
+const string connectionString = "Server=localhost,1433;Databae=BaltaRestore;User ID=SA;Password=lbg@2022";
+
+using (var connection = new SqlConnection(connectionString)) { }
