f8861559c1a8e3ed172a934e8ecef756256a02f1
LisandraGomes <lisandragomes53@gmail.com>
2023-03-21 21:49:14 -0300

    Continuando

--- BaltaDapper/BaltaDapper/Controller/DefaultController.cs
+++ BaltaDapper/BaltaDapper/Controller/DefaultController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BaltaDapper.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,12 +10,112 @@ using System.Threading.Tasks;
 namespace BaltaDapper.Controller
 {
     public class DefaultController
-    {
-   
-        
+    {         
         public static void Get()
         {
+            const string connectionString = @"Server=localhost,1433;
+                          Databae=BaltaRestore;User ID=SA;Password=lbg@2022";
+           
+            var category = new Category();
+           
+            using (var connection = new SqlConnection(connectionString)) 
+            {
+                var categories = connection.Query<Category>("Select [Id], [Title] from [Category]");
+
+                foreach (var item in categories)
+                {
+                    var categoryName = item.Title; 
+                }
+            }
 
         }
+        #region
+        //public static void Post()
+        //{
+        //    const string connectionString = @"Server=localhost,1433;
+        //                  Databae=BaltaRestore;User ID=SA;Password=lbg@2022";
+
+        //    var category = new Category();
+        //    category.Id = Guid.NewGuid();
+        //    category.Title = "";
+        //    category.Description = "";
+
+        //    var insertSql = @"INSERT INTO [Category]
+        //                VALUES(@Id, @Title, @Url, @Summary, @Order, @Description, @Featured)";
+        //    using (var connection = new SqlConnection(connectionString))
+        //    {
+        //        connection.Execute(insertSql, new
+        //        {
+        //           Id = category.Id,
+        //           Title = category.Title,
+        //           Url = category.Url,
+        //           Summary = category.Summary,
+        //           Order = category.Order,
+        //           Description = category.Description,
+        //           Featured = category.Featured
+        //        });
+
+        //        // var categories = connection.Query<Category>("Select [Id], [Title] from [Category]");
+
+        //        //return Ok($"{category.Title}, Foi inserido!");
+        //    }
+
+        //}
+        #endregion
+
+        #region Listar Categorias
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("Select [Id], [Title] from [Category]");
+
+            foreach (var item in categories)
+            {
+                var categoryName = item.Title;
+            }
+        }
+        #endregion
+
+        #region Categoria Nova
+        static void CreateCategory(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "";
+            category.Description = "";
+
+            var insertSql = @"INSERT INTO [Category]
+                        VALUES(@Id, @Title, @Url, @Summary, @Order, @Description, @Featured)";
+           
+            var rows = connection.Execute(insertSql, new
+                {
+                    Id = category.Id,
+                    Title = category.Title,
+                    Url = category.Url,
+                    Summary = category.Summary,
+                    Order = category.Order,
+                    Description = category.Description,
+                    Featured = category.Featured
+                });
+
+                // var categories = connection.Query<Category>("Select [Id], [Title] from [Category]");
+
+                //return Ok($"{category.Title}, Foi inserido!");
+            
+        }
     }
-}
+    #endregion
+
+    #region Atualizar Categoria
+    public static void UpdateCategories(SqlConnection connection)
+    {
+        var updateQuery = "UPDATE [Category] SET [Title] = @title WHERE [Id] = @id";
+        var rows = connection.Execute(updateQuery, new
+        {
+            id = new Guid(""),
+            title = ""
+        });
+
+        //return Ok($"{rows} registros atualizados!");
+    }
+        #endregion
+    }
--- BaltaDapper/BaltaDapper/Models/Category.cs
+++ BaltaDapper/BaltaDapper/Models/Category.cs
@@ -9,8 +9,11 @@ namespace BaltaDapper.Models
     internal class Category
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Summary { get; set; }
+        public int Order { get; set; }
         public string Description { get; set; }
-        public bool Active { get; set; }
+        public bool Featured { get; set; }
     }
 }
