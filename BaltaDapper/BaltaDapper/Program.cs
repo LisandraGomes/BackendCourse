d3a370dd8ca28e7890dbb8629d8fdc54c1bae951
LisandraGomes <lisandragomes53@gmail.com>
2023-03-23 21:34:03 -0300

    Atualizando projeto que foi criado em console, para api web

--- BaltaDapper/BaltaDapper/Program.cs
+++ BaltaDapper/BaltaDapper/Program.cs
@@ -1,6 +1,26 @@
-﻿using Microsoft.Data.SqlClient;
-// See https://aka.ms/new-console-template for more information
+var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 const string connectionString = "Server=localhost,1433;Databae=BaltaRestore;User ID=SA;Password=lbg@2022";
 
-using (var connection = new SqlConnection(connectionString)) { }
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
