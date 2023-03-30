81a2e78dbf3ee4201ce698fa81cb551d4223ade1
Lisandra <lisandragomes53@gmail.com>
2023-03-30 16:37:20 -0300

    Atualizando banco para finalizar o projeto da parte de Dapper.

---
+++ Blog/Program.cs
@@ -0,0 +1,25 @@
+﻿using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+ class Program{ 
+    private const string Connection_String = @"Server=localhost,1433;
+                          Databae=Blog;User ID=SA;Password=lbg@2022";
+
+    public static void ReadUser()
+    {
+        using(var connection = new SqlConnection(Connection_String))
+        {
+            var users = connection.GetAll<User>();
+
+            foreach(var user in users)
+            {
+                Console.WriteLine(user.Name);
+            }
+        }
+    }
+
+ }
+ }
---
+++ Scripts Banco de Dados/02 - Inserção Usuário.sql
@@ -0,0 +1,7 @@
+INSERT INTO [User]
+VALUES('Lisandra Gomes',
+	'lisandragomes53@gmail.com',
+	'Hash',
+	'Desenvolvedora JR',
+	'https://imgs.search.brave.com/3uiFwAtD0XSpeSLFE4XZ8vnK9Nneow8gdAzGGNL99ck/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvcHJl/dmlld3MvMDAwLzU3/NC81MTIvb3JpZ2lu/YWwvdmVjdG9yLXNp/Z24tb2YtdXNlci1p/Y29uLmpwZw',
+	'lisandra-gomes')
